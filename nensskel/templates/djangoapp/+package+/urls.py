3f32e2ed4d2fbc78ebfbe1da06ec73a8e304030f
reinout <reinout@vanrees.org>
2010-02-12 07:00:10 +0000

    django fixes after really using the skel

--- nensskel/templates/djangoapp/+package+/urls.py
+++ nensskel/templates/djangoapp/+package+/urls.py
@@ -1,5 +1,11 @@
 from django.conf.urls.defaults import *
+from django.conf import settings
+from django.contrib import admin
 
+
+admin.autodiscover()
 
 urlpatterns = patterns(
+    '',
+    (r'^admin/', include(admin.site.urls)),
     )
