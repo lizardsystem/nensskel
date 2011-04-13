ac42a9ff4ad4f18220a9287dfc92be6d0360d2f7
gijs <gijs.nijholt@gmail.com>
2011-04-13 09:40:20 +0000

    Adding custom 500 handler.

--- nensskel/templates/djangoapp/+package+/urls.py
+++ nensskel/templates/djangoapp/+package+/urls.py
@@ -1,7 +1,8 @@
 # (c) Nelen & Schuurmans.  GPL licensed, see LICENSE.txt.
-from django.conf.urls.defaults import *
 from django.conf import settings
+from django.conf.urls.defaults import *
 from django.contrib import admin
+from django.views.defaults import page_not_found, server_error
 
 
 admin.autodiscover()
@@ -17,3 +18,19 @@ if settings.DEBUG:
     urlpatterns += patterns('',
         (r'', include('staticfiles.urls')),
     )
+
+
+def handler500(request):
+    """
+    500 error handler which includes ``request`` in the context.
+
+    Templates: `500.html`
+    Context: None
+    """
+    from django.template import Context, loader
+    from django.http import HttpResponseServerError
+
+    t = loader.get_template('500.html') # You need to create a 500.html template.
+    return HttpResponseServerError(t.render(Context({
+        'request': request,
+    })))
