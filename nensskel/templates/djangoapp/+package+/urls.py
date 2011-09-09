1a566d0c66c6aded87436c81190ea2069db9693e
Reinout van Rees <reinout@vanrees.org>
2011-09-09 14:39:58 +0200

    Updated urls.py to use lizard-ui's debugmode_urlpatterns

--- nensskel/templates/djangoapp/+package+/urls.py
+++ nensskel/templates/djangoapp/+package+/urls.py
@@ -1,11 +1,18 @@
 # (c) Nelen & Schuurmans.  GPL licensed, see LICENSE.txt.
 from django.conf.urls.defaults import include
 from django.conf.urls.defaults import patterns
+from django.conf.urls.defaults import url
 from django.contrib import admin
+
+from lizard_ui.urls import debugmode_urlpatterns
 
 admin.autodiscover()
 
 urlpatterns = patterns(
     '',
     (r'^admin/', include(admin.site.urls)),
+    # url(r'^something/',
+    #     direct.import.views.some_method,
+    #     name="name_it"),
     )
+urlpatterns += debugmode_urlpatterns()
